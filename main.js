5805ae3c0bf83af4ec5eadd290cc9660715062f1
EriJohnson <erijohnsons.fernandes@gmail.com>
2019-08-26 05:56:41 -0300

    Implementar validação do formulário de contato

--- main.js
+++ main.js
@@ -9,24 +9,24 @@
   const emailInput = document.getElementById('email');
   const messageInput = document.getElementById('message');
 
-  // "addEventListener" instrui o navegador a executar uma função sempre
-  // que um evento específico acontece. Nesse caso, a função abaixo será
-  // executada toda vez que o botão de envio for pressionado.
   contactForm.addEventListener('submit', function() {
-    // A propriedade "value" contém o valor atual de um input ou textarea
+ 
     const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
 
-    // A propriedade "length" contém o tamanho (em caracteres) de uma
-    // uma string. Toda string contém essa propriedade.
-    const nameLength = nameValue.length;
-
-    // "window" é um objeto global que representa a janela (ou aba) do
-    // navegador que está executando o código do seu site. O método
-    // "alert" simplesmente mostra um aviso para o usuário contendo a
-    // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
-
-    // Altere e complete essa função para validar os campos do formulário
-    // de acordo com as especificações do teste. Boa sorte!
+    if (!nameValue) {
+      window.alert('Preencha com o seu Nome')
+      nameInput.focus()
+    } else if (!emailValue || emailValue.indexOf('@') == -1) {
+      window.alert('Preencha com um email válido.\n\nExemplo:\n\n"exemplo@provedor.com"')
+      emailInput.focus()
+    } else if (!messageValue) {
+      window.alert('Digite uma mensagem!') 
+      messageInput.focus()
+    } else{
+      window.alert('Mensagem enviada com sucesso!')
+    }
+     
   });
 })();
